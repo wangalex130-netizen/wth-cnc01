0a680593b1fb5445d5231d746f0f1bc8cc7851bd
wangalex130-netizen <wang.alex130@gmail.com>
2026-07-27 16:22:20 +0800

    Create cnc_provider.dart

---
+++ lib/services/cnc_provider.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'cnc_service.dart';
+
+class CncProvider extends InheritedNotifier<CncService> {
+  const CncProvider({
+    Super.key,
+    required CncService cncService,
+    required Super.child,
+  }) : super(notifier: cncService);
+
+  static CncService of(BuildContext context) {
+    final provider = context.dependOnInheritedWidgetOfExactType<CncProvider>();
+    assert(provider != null, 'No CncProvider found in context');
+    return provider!.notifier!;
+  }
+}
