56d7ca2ed769bccbcaf10631230f3038c4e03554
wangalex130-netizen <wang.alex130@gmail.com>
2026-07-27 16:24:11 +0800

    Convert SmartCncApp to StatefulWidget and add CncProvider
    Refactor SmartCncApp to use StatefulWidget and integrate CncProvider for CNC service management.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'services/cnc_service.dart';
+import 'services/cnc_provider.dart';
 import 'theme/app_theme.dart';
 import 'views/home_page.dart';
 
@@ -6,16 +8,40 @@ void main() {
   runApp(const SmartCncApp());
 }
 
-class SmartCncApp extends StatelessWidget {
+class SmartCncApp extends StatefulWidget {
   const SmartCncApp({Super.key});
 
   @override
+  State<SmartCncApp> createState() => _SmartCncAppState();
+}
+
+class _SmartCncAppState extends State<SmartCncApp> {
+  final CncService _cncService = CncService();
+
+  @override
+  void initState() {
+    super.initState();
+    // 启动时自动建立模拟通信管道
+    _cncService.connect('COM_VIRTUAL');
+  }
+
+  @override
+  void dispose() {
+    _cncService.disconnect();
+    _cncService.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Smart CNC Pro',
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.darkTheme,
-      home: const HomePage(),
+    return CncProvider(
+      cncService: _cncService,
+      child: MaterialApp(
+        title: 'Smart CNC Pro',
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.darkTheme,
+        home: const HomePage(),
+      ),
     );
   }
 }
